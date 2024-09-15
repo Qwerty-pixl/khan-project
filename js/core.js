35022cf8f4678bbe63db78cd3b311813ff23631d
klaratokonova11 <148875652+Qwerty-pixl@users.noreply.github.com>
2024-09-15 11:21:40 +0600

    and their functions

--- js/core.js
+++ js/core.js
@@ -9,3 +9,24 @@ document.querySelector('.hamburger').addEventListener('click', function() {
     const menu = document.querySelector('.popup-menu');
     menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
 });
+
+
+
+
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3, // Количество видимых слайдов
+    spaceBetween: 30, // Расстояние между слайдами
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2, // Количество слайдов на экранах до 768px
+        },
+        480: {
+            slidesPerView: 1, // Количество слайдов на мобильных экранах
+        }
+    }
+});
